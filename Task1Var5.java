1ae2d5d8dec08346a34ec9d82291898e5dd39bf0
RomashkaHH <Srom.cuzn@yandex.ru>
2026-09-21 17:00:42 +0500

    class→script

--- Task1Var5.java
+++ Task1Var5.java
@@ -1,19 +1,17 @@
-public class Task1Var5 {
-    public static void main(String[] args) {
-        char h = 'o';
-        int number = 6940;
-        String g = "снежинка";
-        String chocolate = "шоколад";
-        String mushroom1 = "гриб";
-        char n = 't';
-        String mushroom2 = "гриб";
-        char h2 = 'U';
-        double n2 = 2110.07644239987039973;
-        double array = 8089.7416602409079392;
-        String m = "торт";
-        float q = 1566.38395733786910724f;
-        char z = 'z';
-        double h3 = 7619.24865735682527731;
-        char words = 'Z';
-    }
+void main(String[] args) {
+    char h = 'o';
+    int number = 6940;
+    String g = "снежинка";
+    String chocolate = "шоколад";
+    String mushroom1 = "гриб";
+    char n = 't';
+    String mushroom2 = "гриб";
+    char h2 = 'U';
+    double n2 = 2110.07644239987039973;
+    double array = 8089.7416602409079392;
+    String m = "торт";
+    float q = 1566.38395733786910724f;
+    char z = 'z';
+    double h3 = 7619.24865735682527731;
+    char words = 'Z';
 }
