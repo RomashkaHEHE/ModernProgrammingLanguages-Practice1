fac4ad3282b3adab02e7211c2456734c3cc1bffc
n1s_01 <2905006@gmail.com>
2026-09-21 16:33:26 +0500

    refactored under task

---
+++ var_5.java
@@ -0,0 +1,19 @@
+public class var_5 {
+    public static void main(String[] args) {
+        String h = "o";
+        int number = 6940;
+        String g = "снежинка";
+        String chocolate = "шоколад";
+        String mushroom1 = "гриб";
+        char n = 't';
+        String mushroom2 = "гриб";
+        char h2 = 'U';
+        double n2 = 2110.07644239987039973;
+        double array = 8089.7416602409079392;
+        String m = "торт";
+        float q = 1566.38395733786910724f;
+        char z = 'z';
+        double h3 = 7619.24865735682527731;
+        char words = 'Z';
+    }
+}
